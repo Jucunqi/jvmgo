0913d02ce7bd333290ed3b40b04b3de09fdc19ce
jucunqi <jucunqi@yeeiee.com>
2025-06-13 09:58:10 +0800

    feat(第二章): 搜索class文件
    实现输出class文件二进制内容

--- ch02/main.go
+++ ch02/main.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Jucunqi/jvmgo/ch02/classpath"
+	"strings"
+)
 
 // 程序执行入口
 func main() {
 
 	// 获取cmd信息
 	cmd := parseCmd()
-
 	// 根据cmd参数决定后面的执行内容
 	if cmd.versionFlag {
-		fmt.Println("version 0.0.1")
+		fmt.Println("version 0.0.2")
 	} else if cmd.helpFlag || cmd.class == "" {
 		printUsage()
 	} else {
@@ -19,7 +22,17 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class:%s args:%v\n",
-		cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.xJreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n",
+		cp, cmd.class, cmd.args)
 
+	classname := strings.ReplaceAll(cmd.class, ".", "/")
+	fmt.Println(classname)
+	data, _, err := cp.ReadClass(classname)
+	if err != nil {
+		fmt.Printf("Cound not find or load main class %s\n", cmd.class)
+		return
+	}
+	// 输出类的内容
+	fmt.Printf("class data:%v\n", data)
 }
